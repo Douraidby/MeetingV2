fc7465bff4168d6399272af3c72e5f9924f4ec17
Douraidby <dbymtl@gmail.com>
2017-03-23 12:35:53 -0400

    29 fichiers

--- app/src/main/java/com/doura/meetingplanner/DatePickerFragment.java
+++ app/src/main/java/com/doura/meetingplanner/DatePickerFragment.java
@@ -3,8 +3,15 @@ package com.doura.meetingplanner;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.Calendar;
 
 /**
@@ -25,6 +32,13 @@ public class DatePickerFragment extends DialogFragment
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+        View eView = inflater.inflate(R.layout.event_layout,null);
+        TextView txt_ddebut = (TextView) eView.findViewById(R.id.ddebut);
+
+        String stringOfDate = day + "/" + month + "/" + year;
+        txt_ddebut.setText(txt_ddebut.getText() + "\n\nFormatted date: " + stringOfDate);
+        Log.d("date debut", (String) txt_ddebut.getText());
 
     }
 }
